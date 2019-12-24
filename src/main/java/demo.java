d81a74eb59d1683ac08c70abb8932aeac0203f80
hongqinghan <hongqinghan>
2019-12-24 16:39:47 +0800

    first blood

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -2,5 +2,6 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("hello my life");
         System.out.println("fly fly fly phoenix fly");
+        System.out.println("fight！");
     }
 }
