af8294e8d930900d92b9d9d21dea2652fa3b6022
hongqinghan <hongqinghan>
2020-04-21 13:48:19 +0800

    ceshi

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -9,20 +9,6 @@ import java.util.List;
 public class test {
 
     public static void main(String[] args) throws Exception {
-
-        HashMap map = new HashMap();
-        List<String> lists = new ArrayList<>();
-        lists.add("1");
-        lists.add("2");
-        lists.add("3");
-        lists.add("4");
-        List<String> lists2 = new ArrayList<>();
-        lists2.addAll(lists);
-        map.put("can",lists2);
-        lists.add("5");
-
-        System.out.println(map.get("can"));
-
-
+        System.out.println(111);
     }
 }
