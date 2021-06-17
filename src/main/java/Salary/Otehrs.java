79383cd566044f08654b364497b02fe9e5438739
洪清涵 <qinghan.hong@hand-china.com>
2021-06-17 20:39:52 +0800

    没有注释

--- src/main/java/Salary/Otehrs.java
+++ src/main/java/Salary/Otehrs.java
@@ -6,7 +6,6 @@ import org.apache.commons.lang.StringUtils;
 public class Otehrs {
 
     public static void main(String[] args) {
-
         System.out.println("FIELD_"+CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, "afterRegister"));
         System.out.println("FIELD_"+CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, "inviteLevel"));
         System.out.println("FIELD_"+CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, "inviteCompanyId"));
